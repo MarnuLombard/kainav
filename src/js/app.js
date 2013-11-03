388cbebb3302ad8863dc295b791e1d2fb5b0371d
MarnuLombard <marnulombard@gmail.com>
2013-11-03 21:53:15 +0200

    Finish first version for client Finish the about.php page Include the contact form in ./includes/static Set up magnific.js Pushed to server Set directoryIndex of remote to placehloder.php

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
 	}
 //--> svg swap
 //
+// Init the magnifid popup modal plugin
+$('#popup-trigger__contact').magnificPopup({
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = 'mfp-move-from-top';
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
+//--> magnific.js
 // Init  the glide js plugin
 	var glide = $('.slider--wrapper').glide({
 		/*arrowRightText: '<img src="img/sliders/slider-arrow--right.svg">',
